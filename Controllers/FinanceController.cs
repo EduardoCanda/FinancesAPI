2ccf3fc0035d9cb0dfe768cd624c4743fc8d312f
Eduardo Canda <eduardocandac@gmail.com>
2021-03-09 00:36:03 -0300

    Update na classe FinanceController.cs

--- Controllers/FinanceController.cs
+++ Controllers/FinanceController.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinancesAPI.Data;
 using FinancesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,12 +14,75 @@ namespace FinancesAPI.Controllers
 
         [HttpGet]
         [Route("")]
-        //public Task<ActionResult<Finance>> GetFinances() {
-        public object GetFinances() {
-            return Ok(new List<Finance>());
+        public ActionResult<List<Finance>> GetFinances([FromServices] DataContext context)
+        {
+            var finances = (context.Finances.ToList());
+            return Ok(finances);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult<Finance> GetFinanceById(int id, [FromServices] DataContext context)
+        {
+            var finance = context.Finances.FirstOrDefault(x => x.Id == id);
+
+            if (finance.Id != id)
+                return NotFound("Finança não encontrada");
+
+            return Ok(finance);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Finance>> AddFinance([FromBody] Finance model, [FromServices] DataContext context)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Finances.Add(model);
+
+                await context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new { Message = "Não foi possível criar a finança", Details = error });
+            }
+
         }
 
 
+        [HttpPut]
+        [Route("")]
+        public async Task<ActionResult<Finance>> UpdateFinance(int id, [FromBody] Finance model, [FromServices] DataContext context)
+        {
+            var finance = context.Finances.FirstOrDefault(x => x.Id == id);
+
+            if (finance.Id != id)
+                return NotFound("Finança não encontrada");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            finance.SetCategory(model.Category);
+            finance.SetValue(model.Value);
+
+            try
+            {
+                await context.SaveChangesAsync();
+
+                return Ok(model);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new { Message = "Não foi possível atualizar a finança", Details = error });
+            }
+
+        }
 
     }
 }
