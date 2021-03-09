42624ffd08cf9572b4b3cf0dca58c0b3b4312e38
Eduardo Canda <eduardocandac@gmail.com>
2021-03-09 00:36:32 -0300

    Update no model Finance.cs

--- Models/Finance.cs
+++ Models/Finance.cs
@@ -6,22 +6,32 @@ namespace FinancesAPI.Models
 {
     public class Finance
     {
-        public Finance(ECategory category, decimal value, DateTime date)
+        public Finance(ECategory category, decimal value)
         {
             Category = category;
             Value = value;
-            Date = date;
+            Date = DateTime.Now;
         }
 
         [Key]
-        public int Id { get; set; }
-        
+        public int Id { get; }
+
         [Required(ErrorMessage = "A categoria é obrigatória")]
-        public ECategory Category { get; set; }
-        
+        public ECategory Category { get; private set; }
+
         [Required(ErrorMessage = "O valor é obrigatório")]
-        public decimal Value { get; set; }
-        public DateTime Date { get; set; }
+        public decimal Value { get; private set; }
+        public DateTime Date { get; }
+
+        public void SetValue(decimal value)
+        {
+            Value = value;
+        }
+
+        public void SetCategory(ECategory category)
+        {
+            Category = category;
+        }
 
     }
 }
